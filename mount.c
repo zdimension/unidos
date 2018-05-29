d440f35718a859756e2a16fc0b9a0ae499c06206
zdimension <zippedfire@free.fr>
2018-05-29 21:35:00 +0200

    mount

---
+++ mount.c
@@ -0,0 +1,115 @@
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "mount.h"
+#include "defs.h"
+#include "dospath.h"
+
+struct mount_point
+{
+    uint8_t drive;
+    char path[514];
+    struct mount_point* prev;
+    struct mount_point* next;
+} *mount_points = NULL;
+
+uint8_t mount_remove(uint8_t drive)
+{
+    for(struct mount_point* cur = mount_points; cur; cur = cur->next)
+    {
+        if (cur->drive == drive)
+        {
+            if (cur->prev)
+            {
+                cur->prev->next = cur->next;
+            }
+
+            if (cur->next)
+            {
+                cur->next->prev = cur->prev;
+            }
+
+            free(cur);
+
+            return ERR_SUCCESS;
+        }
+    }
+
+    return ERR_INVALID_DRIVE_SPECIFIED;
+}
+
+uint8_t mount_add(uint8_t drive, char* path)
+{
+    printf("Adding mount point %c = %s\n", drive + 'A', path);
+
+    for(struct mount_point* cur = mount_points; cur; cur = cur->next)
+    {
+        if (cur->drive == drive)
+        {
+            printf("Drive %c already mounted (to %s)\n", cur->drive + 'A', cur->path);
+            return ERR_INVALID_DRIVE_SPECIFIED;
+        }
+    }
+
+    struct mount_point* pnt = malloc(sizeof(struct mount_point));
+    memset(pnt, 0, sizeof(struct mount_point));
+    pnt->drive = drive;
+    strcpy(pnt->path, path);
+
+    if (!mount_points)
+    {
+        mount_points = pnt;
+        return ERR_SUCCESS;
+    }
+
+    struct mount_point* cur = mount_points;
+    while (cur->next) cur = cur->next;
+
+    cur->next = pnt;
+    pnt->prev = cur;
+
+    return ERR_SUCCESS;
+}
+
+uint8_t mount_str_to_real(char* inp, char* buf)
+{
+    struct dospath path;
+    path_parse(inp, &path);
+    return mount_to_real(&path, buf);
+}
+
+uint8_t mount_to_real(struct dospath* path, char* buf)
+{
+    struct dospath real;
+    path_absolute(*path, &real);
+
+    struct mount_point* good = NULL;
+
+    for(struct mount_point* cur = mount_points; cur; cur = cur->next)
+    {
+        if (cur->drive == real.drive)
+        {
+            good = cur;
+            break;
+        }
+    }
+
+    if (!good)
+    {
+        printf("Drive %c is not mounted\n", real.drive + 'A');
+        return ERR_INVALID_DRIVE_SPECIFIED;
+    }
+
+    buf[0] = 0;
+
+    strcat(buf, good->path);
+
+    for (int i = 0; i < real.depth; i++)
+    {
+        strcat(buf, "/");
+        strcat(buf, real.path[i]);
+    }
+
+    return ERR_SUCCESS;
+}
---
+++ mount.h
@@ -0,0 +1,41 @@
+#ifndef UNIDOS_MOUNT_H
+#define UNIDOS_MOUNT_H
+
+#include "dospath.h"
+
+enum DRIVES
+{
+    DRIVE_A,
+    DRIVE_B,
+    DRIVE_C,
+    DRIVE_D,
+    DRIVE_E,
+    DRIVE_F,
+    DRIVE_G,
+    DRIVE_H,
+    DRIVE_I,
+    DRIVE_J,
+    DRIVE_K,
+    DRIVE_L,
+    DRIVE_M,
+    DRIVE_N,
+    DRIVE_O,
+    DRIVE_P,
+    DRIVE_Q,
+    DRIVE_R,
+    DRIVE_S,
+    DRIVE_T,
+    DRIVE_U,
+    DRIVE_V,
+    DRIVE_W,
+    DRIVE_X,
+    DRIVE_Y,
+    DRIVE_Z
+};
+
+uint8_t mount_remove(uint8_t drive);
+uint8_t mount_add(uint8_t drive, char* path);
+uint8_t mount_str_to_real(char* inp, char* buf);
+uint8_t mount_to_real(struct dospath* path, char* buf);
+
+#endif
